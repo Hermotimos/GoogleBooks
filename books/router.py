c61ad12d0e0626223a7783e4241e4904db7af1bc
Hermotimos <lukas.kozicki@gmail.com>
2020-02-15 10:14:44 +0100

    for loop in router.py for printing out urls in router.urls

--- books/router.py
+++ books/router.py
@@ -3,6 +3,9 @@ from rest_framework.routers import DefaultRouter
 from books.api.viewsets import AuthorViewSet, BookViewSet, LanguageViewSet
 
 router = DefaultRouter()
-router.register(r'authors', AuthorViewSet)
-router.register(r'books', BookViewSet)
-router.register(r'languages', LanguageViewSet)
+router.register('authors', AuthorViewSet)
+router.register('books', BookViewSet)
+router.register('languages', LanguageViewSet)
+
+for url in router.urls:
+    print(url)
