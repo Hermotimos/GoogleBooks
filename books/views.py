a78a1ab0b67e6f8bd041ba70514a18f485963e4b
Hermotimos <lukas.kozicki@gmail.com>
2020-02-15 13:31:22 +0100

    + query_debugger decorator

--- books/views.py
+++ books/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from books.filters import BookFilter
 from books.forms import BooksSearchForm, BooksAddForm
 from books.models import Book
+from books_project.utils import query_debugger
 
 
+@query_debugger
 def books_list_view(request):
     books = Book.objects.all()
     form = BooksSearchForm()
@@ -18,6 +20,7 @@ def books_list_view(request):
     return render(request, 'books_list.html', context)
 
 
+@query_debugger
 def books_add_view(request):
     form = BooksAddForm()
     context = {
