08ac045835f5f72bc42869e3e697571cc0233446
Hermotimos <lukas.kozicki@gmail.com>
2020-02-15 07:40:29 +0100

    BooksSearchForm + Bootstrap grid in tempaltes

--- books/forms.py
+++ books/forms.py
@@ -3,7 +3,13 @@ from django import forms
 from books.models import Book
 
 
-class BookAddForm(forms.ModelForm):
+class BooksSearchForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['authors', 'title', 'language']
+
+
+class BooksAddForm(forms.ModelForm):
     class Meta:
         model = Book
         exclude = []
--- books/views.py
+++ books/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 
-from books.forms import BookAddForm
+from books.forms import BooksSearchForm, BooksAddForm
 from books.models import Author, Book, Language
-
 
 
 def books_list_view(request):
     books = Book.objects.all()
 
+    form = BooksSearchForm()
+
     context = {
-        'books': books
+        'books': books,
+        'form': form
     }
     return render(request, 'books_list.html', context)
 
 
 def books_add_view(request):
-    form = BookAddForm()
+    form = BooksAddForm()
     context = {
         'form': form
     }
