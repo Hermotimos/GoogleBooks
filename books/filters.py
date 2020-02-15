0b9abdef1ab441b78288b3207ea498a12359ad4b
Hermotimos <lukas.kozicki@gmail.com>
2020-02-15 13:14:59 +0100

    more expressive filters in books.filters.py

--- books/filters.py
+++ books/filters.py
@@ -4,14 +4,12 @@ from books.models import Author, Book, Language
 
 
 class BookFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains')
     authors = django_filters.ModelMultipleChoiceFilter(queryset=Author.objects.all())
     language = django_filters.ModelMultipleChoiceFilter(queryset=Language.objects.all())
+    pub_year__gte = django_filters.NumberFilter(field_name='pub_year', lookup_expr='gte')
+    pub_year__lte = django_filters.NumberFilter(field_name='pub_year', lookup_expr='lte')
 
     class Meta:
         model = Book
-        fields = {
-            'title': ['icontains'],
-            'authors': [],
-            'language': [],
-            'pub_year': ['lte', 'gte']
-        }
+        fields = ['title', 'authors', 'language', 'pub_year__gte', 'pub_year__gte']
