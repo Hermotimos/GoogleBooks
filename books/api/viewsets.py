ffc92ae428e29d712f4795e2d85b4787ff551e75
Hermotimos <lukas.kozicki@gmail.com>
2020-02-15 10:14:04 +0100

    filter added to rest api

--- books/api/viewsets.py
+++ books/api/viewsets.py
@@ -1,29 +1,53 @@
+from django_filters import rest_framework as filters
 from rest_framework import viewsets
-from rest_framework.views import APIView
-from rest_framework.response import Response
 
 from books.api.serializers import AuthorSerializer, BookSerializer, LanguageSerializer
 from books.models import Author, Book, Language
 
 
-# class BookList(APIView):
+# class AuthorFilter(filters.FilterSet):
+#     name = filters.CharFilter(lookup_expr='icontains')
 #
-#     def get(self, request):
-#         books = Book.objects.all()
-#         serializer = BookSerializer(books, many=True, context={'request': request})
-#         return Response(serializer.data)
+#     class Meta:
+#         model = Author
+#         fields = ['name']
+
+
+class BookFilter(filters.FilterSet):
+
+    # TODO add pub_date and DateFromToRangeFilter (requires DateField)
+
+    title = filters.CharFilter(lookup_expr='icontains')
+    authors = filters.ModelMultipleChoiceFilter(field_name='authors__name',  to_field_name='name',
+                                                queryset=Author.objects.all())
+
+    class Meta:
+        model = Book
+        fields = ['title', 'authors', 'language']   # TODO add pub_date and DateFromToRangeFilter (requires DateField)
+
+
+# class LanguageFilter(filters.FilterSet):
+#     name = filters.CharFilter(lookup_expr='icontains')
+#
+#     class Meta:
+#         model = Language
+#         fields = ['name']
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    # filterset_class = AuthorFilter
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_fields = ['title']
+    filterset_class = BookFilter
 
 
 class LanguageViewSet(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    # filterset_class = LanguageFilter
