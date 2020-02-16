ac7660bd3aa8cb94df90e87d91736bd032c6f67d
Hermotimos <lukas.kozicki@gmail.com>
2020-02-16 16:49:06 +0100

    books_add_view() + forms + template

--- books/forms.py
+++ books/forms.py
@@ -1,7 +1,7 @@
 import datetime
 from django import forms
 
-from books.models import Author, Book
+from books.models import Author, Book, Language
 
 
 class BooksSearchForm(forms.ModelForm):
@@ -10,42 +10,50 @@ class BooksSearchForm(forms.ModelForm):
         fields = ['authors', 'title', 'language']
 
 
-class AuthorForm(forms.ModelForm):
-    name = forms.CharField(max_length=100, required=False)
-
-    class Meta:
-        model = Author
-        exclude = []
+class FirstAuthorForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
 
     def __init__(self, *args, **kwargs):
-        super(AuthorForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+        self.fields['name'].label = 'Author'
+
+
+class AuthorForm(forms.Form):
+    name = forms.CharField(max_length=100, required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.fields['name'].label = 'Author'
 
 
 AuthorFormSet = forms.formset_factory(AuthorForm, extra=4)
 
 
+class LanguageForm(forms.Form):
+    code = forms.CharField(min_length=2, max_length=2)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['code'].label = '2-Digit language code'
+
+
 def get_current_year():
     return datetime.datetime.now().year
 
 
-class BooksAddForm(forms.ModelForm):
-    authors = forms.CharField(max_length=100)
+class BookForm(forms.ModelForm):
     pages = forms.IntegerField(min_value=1)
-    language = forms.CharField(min_length=2, max_length=2)        # TODO use this to populate related model
     isbn_10 = forms.CharField(min_length=10, max_length=10)
     isbn_13 = forms.CharField(min_length=13, max_length=13)
 
     year = forms.IntegerField(min_value=1450, max_value=get_current_year)
-    month = forms.IntegerField(min_value=1, max_value=12)
-    day = forms.IntegerField(min_value=1, max_value=31)
+    month = forms.IntegerField(min_value=1, max_value=12, required=False)
+    day = forms.IntegerField(min_value=1, max_value=31, required=False)
 
     class Meta:
         model = Book
-        exclude = ['pub_year']
+        exclude = ['authors', 'language', 'pub_year']
 
     def __init__(self, *args, **kwargs):
-        super(BooksAddForm, self).__init__(*args, **kwargs)
-        self.fields['authors'].label = 'Author'
-
+        super().__init__(*args, **kwargs)
         self.fields['title'].widget.attrs = {'size': 70}
--- books/views.py
+++ books/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.contrib import messages
 from books.filters import BookFilter
-from books.forms import BooksSearchForm, BooksAddForm, AuthorFormSet
-from books.models import Book
+from books.forms import BooksSearchForm, BookForm, FirstAuthorForm, AuthorFormSet, LanguageForm
+from books.models import Author, Book, Language
 from books_project.utils import query_debugger
 
 
@@ -22,11 +23,56 @@ def books_list_view(request):
 
 @query_debugger
 def books_add_view(request):
-    form = BooksAddForm()
-    author_formset = AuthorFormSet()
+    if request.method == 'POST':
+        first_author_form = FirstAuthorForm(data=request.POST)
+        # authors_formset = AuthorFormSet(data=request.POST)
+        book_form = BookForm(data=request.POST)
+        language_form = LanguageForm(data=request.POST)
+
+        if first_author_form.is_valid() and language_form.is_valid():
+            first_author_name = first_author_form.cleaned_data['name']
+            first_author, _ = Author.objects.get_or_create(name=first_author_name)
+
+            language_code = language_form.cleaned_data['code']
+            language, _ = Language.objects.get_or_create(code=language_code)
+
+            book = book_form.save(commit=False)
+            book.language = language
+            book = book_form.save()
+            book.authors.add(first_author)
+            # if more_authors:
+            #     book.authors.add(*list(more_authors))
+
+            print(book_form.cleaned_data)
+
+            year = book_form.cleaned_data['year']
+            month = book_form.cleaned_data['month'] or None
+            day = book_form.cleaned_data['day'] or None
+
+            if month and day:
+                book.pub_year = str(year) + '-' + str(month) + '-' + str(day)
+            elif month:
+                book.pub_year = str(year) + '-' + str(month)
+            else:
+                book.pub_year = str(year)
+
+            book.save()
+
+            messages.info(request, f'New book has been added!')
+            return redirect('add')
+        else:
+            print('not valid')
+
+    else:
+        first_author_form = FirstAuthorForm()
+        # authors_formset = AuthorFormSet()
+        book_form = BookForm()
+        language_form = LanguageForm()
 
     context = {
-        'form': form,
-        'author_formset': author_formset
+        'first_author_form': first_author_form,
+        # 'authors_formset': authors_formset,
+        'book_form': book_form,
+        'language_form': language_form
     }
     return render(request, 'books_add.html', context)
