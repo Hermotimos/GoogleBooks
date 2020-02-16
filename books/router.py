f88900f983592cf48f3b8f6261c2bdb34b6b7524
Hermotimos <lukas.kozicki@gmail.com>
2020-02-16 20:21:02 +0100

    minor

--- books/router.py
+++ books/router.py
@@ -3,9 +3,7 @@ from rest_framework.routers import DefaultRouter
 from books.api.viewsets import AuthorViewSet, BookViewSet, LanguageViewSet
 
 router = DefaultRouter()
+
 router.register('authors', AuthorViewSet)
 router.register('books', BookViewSet)
 router.register('languages', LanguageViewSet)
-
-for url in router.urls:
-    print(url)
