175a5225764d56a7e329215b058066ef70f1b794
Hermotimos <lukas.kozicki@gmail.com>
2020-02-17 16:05:58 +0100

    BooksAddViewTest

--- books/tests.py
+++ books/tests.py
@@ -3,6 +3,8 @@ from django.urls import resolve, reverse
 
 from books import views
 from books.filters import BookFilter
+from books.forms import (FirstAuthorForm, AuthorForm, AuthorFormSet, BookForm,
+                         LanguageForm)
 from books.models import Author, Book, Language
 
 
@@ -109,3 +111,121 @@ class BooksListViewTest(TestCase):
             check_filtering(self, book, in_qs, out_qs)
 
 
+class BooksAddViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.url = reverse('add')
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_resolves_view(self):
+        view = resolve('/add/')
+        self.assertEqual(view.func, views.books_add_view)
+
+    def test_csrf(self):
+        response = self.client.get(self.url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+        
+    def test_contains_forms(self):
+        response = self.client.get(self.url)
+        
+        first_author_form = response.context.get('first_author_form')
+        authors_fs = response.context.get('authors_fs')
+        book_form = response.context.get('book_form')
+        language_form = response.context.get('language_form')
+        
+        self.assertIsInstance(first_author_form, FirstAuthorForm)
+        self.assertIsInstance(authors_fs, AuthorFormSet)
+        self.assertIsInstance(book_form, BookForm)
+        self.assertIsInstance(language_form, LanguageForm)
+
+    def test_valid_post_data(self):
+        data = {
+            # first_author_form
+            'name': 'Test author 1',
+            
+            # authors_fs --> 'form-0-name', ..., 'form-3-name' + ManagementForm
+            'form-0-name': 'Test author 2',
+            'form-1-name': '',
+            'form-2-name': '',
+            'form-3-name': '',
+            'form-TOTAL_FORMS': ['4'],
+            'form-INITIAL_FORMS': ['0'],
+            'form-MIN_NUM_FORMS': ['0'],
+            'form-MAX_NUM_FORMS': ['1000'],
+            
+            # language_form
+            'code': 'xx',
+            
+            # book_form
+            'title': 'Test title 1',
+            'year': '2000',
+            'month': '',
+            'day': '',
+            'pages': '200',
+            'isbn_10': '0575079215',
+            'isbn_13': '9780575079212',
+            'cover_url': 'http://127.0.0.1:8000/',
+        }
+        self.assertTrue(Book.objects.count() == 0)
+        self.assertTrue(Author.objects.count() == 0)
+        self.assertTrue(Language.objects.count() == 0)
+        
+        self.client.post(self.url, data)
+        
+        self.assertTrue(Book.objects.count() == 1)
+        self.assertTrue(Author.objects.count() == 2)
+        self.assertTrue(Language.objects.count() == 1)
+
+    def test_invalid_post_data(self):
+        data = {
+            # first_author_form
+            'name': 'len(name) > 100' * 100,
+        
+            # authors_fs --> 'form-0-name', ..., 'form-3-name' + ManagementForm
+            'form-0-name': 'len(name) > 100' * 100,
+            'form-1-name': '',
+            'form-2-name': '',
+            'form-3-name': '',
+            'form-TOTAL_FORMS': ['4'],
+            'form-INITIAL_FORMS': ['0'],
+            'form-MIN_NUM_FORMS': ['0'],
+            'form-MAX_NUM_FORMS': ['1000'],
+        
+            # language_form
+            'code': 'len(str) > 2',
+        
+            # book_form
+            'title': 'len(title) > 500' * 500,
+            'year': '2500',
+            'month': '',
+            'day': '',
+            'pages': '-200',
+            'isbn_10': '11111111111111111111',
+            'isbn_13': '11111111111111111111',
+            'cover_url': 'not an url',
+        }
+        self.assertTrue(Book.objects.count() == 0)
+        self.assertTrue(Author.objects.count() == 0)
+        self.assertTrue(Language.objects.count() == 0)
+        
+        response = self.client.post(self.url, data)
+        
+        # should show the form again, not redirect
+        self.assertEquals(response.status_code, 200)
+        
+        first_author_form = response.context.get('first_author_form')
+        authors_fs = response.context.get('authors_fs')
+        language_form = response.context.get('language_form')
+        book_form = response.context.get('book_form')
+
+        self.assertTrue(first_author_form.errors)
+        self.assertTrue(authors_fs.errors)
+        self.assertTrue(language_form.errors)
+        self.assertTrue(book_form.errors)
+
+        self.assertTrue(Book.objects.count() == 0)
+        self.assertTrue(Author.objects.count() == 0)
+        self.assertTrue(Language.objects.count() == 0)
