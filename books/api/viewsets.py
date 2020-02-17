74b646621ff888820af2526ea724bc5c6db37182
Hermotimos <lukas.kozicki@gmail.com>
2020-02-17 10:32:53 +0100

    minor

--- books/api/viewsets.py
+++ books/api/viewsets.py
@@ -8,8 +8,7 @@ from books.models import Author, Book, Language
 
 class BookFilter(filters.FilterSet):
     title = filters.CharFilter(lookup_expr='icontains')
-    authors = filters.ModelMultipleChoiceFilter(
-        queryset=Author.objects.all())
+    authors = filters.ModelMultipleChoiceFilter(queryset=Author.objects.all())
     language = filters.ModelMultipleChoiceFilter(
         queryset=Language.objects.all())
     pub_date__gte = filters.NumberFilter(field_name='pub_date',
