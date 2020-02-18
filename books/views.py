a26209beb51c6bdfe7e52e70fbab23a4518e28e6
Hermotimos <lukas.kozicki@gmail.com>
2020-02-18 21:29:05 +0100

    books_import_view - working version

--- books/views.py
+++ books/views.py
@@ -1,7 +1,10 @@
-import requests
+import json
 
 from django.shortcuts import render, redirect
 from django.contrib import messages
+
+import requests
+from requests import Request
 
 from books.filters import BookFilter
 from books.forms import (BookForm, FirstAuthorForm, AuthorFormSet,
@@ -14,8 +17,81 @@ def books_import_view(request):
         form = BookImportForm(data=request.POST)
         
         if form.is_valid():
-            pass
+            terms = {}
+            for k, v in form.cleaned_data.items():
+                if v:
+                    terms[k] = v
+            
+            req = Request('POST',
+                          'https://www.googleapis.com/books/v1/volumes',
+                          params=terms)
+            prep = req.prepare()
+            url = prep.url.replace('&', '+')
+            response = requests.get(url)
+            
+            data = json.loads(response.text)
+            added_cnt = 0
+            for item in data['items']:
+                try:
+                    title = item['volumeInfo']['title']
+                except KeyError:
+                    title = ''
+                
+                try:
+                    authors_list = item['volumeInfo']['authors']
+                except KeyError:
+                    authors_list = []
     
+                try:
+                    pub_date = item['volumeInfo']['publishedDate']
+                except KeyError:
+                    pub_date = ''
+                    
+                try:
+                    pages = item['volumeInfo']['pageCount']
+                except KeyError:
+                    pages = None
+
+                isbn = ''
+                try:
+                    for elem in item['volumeInfo']['industryIdentifiers']:
+                        if elem['type'] == 'ISBN_13':
+                            isbn = elem['identifier']
+                            break
+                        elif elem['type'] == 'ISBN_10':
+                            isbn = elem['identifier']
+                except KeyError:
+                    isbn = ''
+
+                try:
+                    language = item['volumeInfo']['language']
+                except KeyError:
+                    language = ''
+                    
+                try:
+                    cover_url = item['volumeInfo']['imageLinks']['thumbnail']
+                except KeyError:
+                    cover_url = ''
+
+                language_obj, _ = Language.objects.get_or_create(code=language)
+                book, added = Book.objects.get_or_create(
+                    title=title,
+                    pub_date=pub_date,
+                    pages=pages,
+                    language=language_obj,
+                    isbn=isbn,
+                    cover_url=cover_url
+                )
+                if added:
+                    added_cnt += 1
+                    authors_objs = []
+                    for name in authors_list:
+                        author, _ = Author.objects.get_or_create(name=name)
+                        authors_objs.append(author)
+                    book.authors.set(authors_objs)
+
+            messages.info(request,
+                          f'{added_cnt} books added to your collection!')
     else:
         form = BookImportForm()
 
