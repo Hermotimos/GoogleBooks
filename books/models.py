8d4331bb0677f9899ec868ab550df509bfb1a33d
Hermotimos <lukas.kozicki@gmail.com>
2020-02-20 12:59:06 +0100

    save() on Book model - only isbn field as NULL

--- books/models.py
+++ books/models.py
@@ -54,20 +54,14 @@ class Book(models.Model):
 
     def save(self, *args, **kwargs):
         """
-        Save blank fields as None instead of empty string ''.
+        Save blank field 'isbn' as None instead of empty string ''.
         
         Overrides default saving of blank fields as empty strings in database
-        and enforces saving them as NULLs (None is translated to NULL).
+        and enforces saving as NULLs (None is translated to NULL).
         Database treats two empty strings as equal, which isn't true for NULLs,
-        so this override enables saving of multiple Book objects with some of
-        their fields empty while using those fields for UNIQUE check.
+        so this override enables saving of multiple Book objects with 'isbn'
+        field empty while using this field for UNIQUE check.
         """
-        if not self.pages:
-            self.pages = None
         if not self.isbn:
             self.isbn = None
-        if not self.cover_url:
-            self.cover_url = None
-        if not self.cover_url:
-            self.cover_url = None
         super().save(*args, **kwargs)
